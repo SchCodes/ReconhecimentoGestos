cf9457d9cea9650294e1326bf3dd3aa3aa975166
Ericson Schmidt Bicalho <160190755+SchCodes@users.noreply.github.com>
2024-12-02 17:59:01 -0300

    Update real_time_detection.py

--- real_time_detection.py
+++ real_time_detection.py
@@ -1,28 +1,45 @@
-
 import cv2
 import tensorflow as tf
 import numpy as np
+import mediapipe as mp
+
+categories = ['positivo', 'indicador', 'shaka', 'punho', 'rock']
 
 def load_model(model_path):
     return tf.keras.models.load_model(model_path)
 
+def preprocess_image(image):
+    image = cv2.resize(image, (224, 224))
+    image = image / 255.0
+    image = np.expand_dims(image, axis=0)
+    return image
+
 def detect_gestures(model):
     cap = cv2.VideoCapture(0)
+    mp_hands = mp.solutions.hands
+    hands = mp_hands.Hands(max_num_hands=1)
     while True:
         ret, frame = cap.read()
         if not ret:
             break
 
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        resized = cv2.resize(gray, (128, 128)) / 255.0
-        input_data = np.expand_dims(resized, axis=(0, -1))
+        rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        results = hands.process(rgb)
 
-        predictions = model.predict(input_data)
-        gesture = np.argmax(predictions)
-        confidence = np.max(predictions)
+        if results.multi_hand_landmarks:
+            for hand_landmarks in results.multi_hand_landmarks:
+                mp.solutions.drawing_utils.draw_landmarks(frame, hand_landmarks, mp_hands.HAND_CONNECTIONS)
 
-        cv2.putText(frame, f"Gesture: {gesture} ({confidence:.2f})", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2)
-        cv2.imshow("Gesture Detection", frame)
+            preprocessed_image = preprocess_image(frame)
+            predictions = model.predict(preprocessed_image)
+            predicted_index = np.argmax(predictions, axis=1)[0]
+            predicted_category = categories[predicted_index]
+
+            print("Previsões: ", predictions)
+
+            cv2.putText(frame, f'Prediction: {predicted_category}', (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2, cv2.LINE_AA)
+
+        cv2.imshow('Real-Time Detection', frame)
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
@@ -30,6 +47,6 @@ def detect_gestures(model):
     cap.release()
     cv2.destroyAllWindows()
 
-# Exemplo de uso
-model = load_model("models/cnn_model.h5")
-detect_gestures(model)
+if __name__ == "__main__":
+    model = load_model('models/cnn_model.h5')
+    detect_gestures(model)
