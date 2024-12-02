1b3e1280880488c99406296c0e8f77d325adebd6
Ericson Schmidt Bicalho <160190755+SchCodes@users.noreply.github.com>
2024-12-02 18:04:29 -0300

    Update train_model.py

--- train_model.py
+++ train_model.py
@@ -2,12 +2,14 @@
 import tensorflow as tf
 from tensorflow.keras import layers, models
 
-def create_model(input_shape=(128, 128, 1), num_classes=3):
+def create_model(input_shape=(224, 224, 3), num_classes=5):
     model = models.Sequential([
         layers.Conv2D(32, (3, 3), activation='relu', input_shape=input_shape),
-        layers.MaxPooling2D((2, 2)),
+        layers.MaxPooling2D((2, 2), strides=2),
         layers.Conv2D(64, (3, 3), activation='relu'),
-        layers.MaxPooling2D((2, 2)),
+        layers.MaxPooling2D((2, 2), strides=2),
+        layers.Conv2D(128, (3, 3), activation='relu'),
+        layers.MaxPooling2D((2, 2), strides=2),
         layers.Flatten(),
         layers.Dense(128, activation='relu'),
         layers.Dense(num_classes, activation='softmax')
@@ -15,11 +17,11 @@ def create_model(input_shape=(128, 128, 1), num_classes=3):
     model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
     return model
 
-def train_model(data_dir, model_save_path="models/cnn_model.h5"):
-    train_ds = tf.keras.utils.image_dataset_from_directory(data_dir, batch_size=32, image_size=(128, 128), color_mode='grayscale')
+def train_model(data_dir, model_save_path="models/cnn_model.keras"):
+    train_ds = tf.keras.utils.image_dataset_from_directory(data_dir, batch_size=32, image_size=(224, 224), color_mode='rgb')
     model = create_model()
     model.fit(train_ds, epochs=10)
     model.save(model_save_path)
 
 # Exemplo de uso
-train_model("data/processed")
+train_model("data/processed/categorias")
